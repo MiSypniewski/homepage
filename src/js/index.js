baabf7cccb22e23f6327719d94922f326c57977b
MiSypniewski <mi.sypniewski@gmail.com>
2020-08-11 19:17:38 +0200

    FIX: change animation

--- src/js/index.js
+++ src/js/index.js
@@ -1,25 +1,40 @@
-import '../scss/main.scss';
-import { gsap } from 'gsap';
-import { ScrollTrigger } from 'gsap/ScrollTrigger';
+import "../scss/main.scss";
+import { gsap } from "gsap";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 gsap.registerPlugin(ScrollTrigger);
 
-console.log('HELLO 👋')
+console.log("HELLO 👋");
 //https://api.github.com/users/:username/repos
 
 const projectsWrapperHTML = document.querySelector(".projects__wrapper--js");
-const skillsHTML = document.querySelector(".skills");
-const projectsHTML = document.querySelector(".projects");
-const heroBubbleHTML = document.querySelector(".hero__bubble");
+
+const animationSections = document.querySelectorAll(".animation--js");
+
 const heroHandHTML = document.querySelector(".hero__hand");
-const contactHTML = document.querySelector(".contact");
+const heroBubbleHTML = document.querySelector(".hero__bubble");
 
-gsap.fromTo(heroBubbleHTML, { scale: 0 }, { scale: 1, transformOrigin: '0% 100%', duration: 0.3 });
-gsap.fromTo(heroHandHTML, { rotate: -10 }, { rotate: 10, ease: "easeInOut", transformOrigin: '80% 80%', duration: 0.3, repeat: -1, yoyo: true });
-gsap.fromTo(skillsHTML.children, { y: '+=100', opacity: 0 }, { y: 0, opacity: 1, stagger: 0.3, duration: 1, scrollTrigger: { trigger: '.skills', start: "top 40%" } });
-gsap.fromTo(projectsHTML.children, { y: '+=100', opacity: 0 }, { y: 0, opacity: 1, stagger: 0.3, duration: 1, scrollTrigger: { trigger: '.projects', start: "top 40%" } });
-gsap.fromTo(contactHTML.children, { y: '+=100', opacity: 0 }, { y: 0, opacity: 1, stagger: 0.3, duration: 1, scrollTrigger: { trigger: '.contact', start: "50% 10%" } });
+gsap.fromTo(heroBubbleHTML, { scale: 0 }, { scale: 1, transformOrigin: "0% 100%", duration: 0.3, delay: 0.5 });
+gsap.fromTo(
+  heroHandHTML,
+  { rotate: -10 },
+  { rotate: 10, ease: "easeInOut", transformOrigin: "80% 80%", duration: 0.3, repeat: -1, yoyo: true }
+);
 
+animationSections.forEach((section) => {
+  gsap.fromTo(
+    section.children,
+    { y: "+=100", opacity: 0 },
+    {
+      y: 0,
+      opacity: 1,
+      stagger: 0.3,
+      duration: 0.8,
+      ease: "easeInOut",
+      scrollTrigger: { trigger: section, start: "top 40%" },
+    }
+  );
+});
 
 const projectScheme = (name, description, githuburl, homepage) => {
   return `<article class="project">
@@ -48,19 +63,15 @@ const projectScheme = (name, description, githuburl, homepage) => {
             </p>
           </div>
         </article>`;
-}
+};
 
 fetch("https://api.github.com/users/misypniewski/repos")
-  .then(resp => resp.json())
-  .then(resp => {
-    resp.forEach(item => {
+  .then((resp) => resp.json())
+  .then((resp) => {
+    resp.forEach((item) => {
       if (item.description != null) {
         projectsWrapperHTML.innerHTML += projectScheme(item.name, item.description, item.html_url, item.homepage);
       }
-
     });
   })
   .catch((error) => console.log(error));
-
-
-
