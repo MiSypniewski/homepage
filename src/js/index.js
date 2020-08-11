37451321cf930a9a75a566c5f91437e66c3ff662
Michał Sypniewski <mi.sypniewski@gmail.com>
2020-08-11 15:04:54 +0200

    FEAT: add animations with GSAP

--- src/js/index.js
+++ src/js/index.js
@@ -1,10 +1,24 @@
 import '../scss/main.scss';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
+
+gsap.registerPlugin(ScrollTrigger);
 
 console.log('HELLO 👋')
-
 //https://api.github.com/users/:username/repos
 
-const projectsHTML = document.querySelector(".projects__wrapper--js");
+const projectsWrapperHTML = document.querySelector(".projects__wrapper--js");
+const skillsHTML = document.querySelector(".skills");
+const projectsHTML = document.querySelector(".projects");
+const heroBubbleHTML = document.querySelector(".hero__bubble");
+const heroHandHTML = document.querySelector(".hero__hand");
+const contactHTML = document.querySelector(".contact");
+
+gsap.fromTo(heroBubbleHTML, { scale: 0 }, { scale: 1, transformOrigin: '0% 100%', duration: 0.3 });
+gsap.fromTo(heroHandHTML, { rotate: -10 }, { rotate: 10, ease: "easeInOut", transformOrigin: '80% 80%', duration: 0.3, repeat: -1, yoyo: true });
+gsap.fromTo(skillsHTML.children, { y: '+=100', opacity: 0 }, { y: 0, opacity: 1, stagger: 0.3, duration: 1, scrollTrigger: { trigger: '.skills', start: "top 40%" } });
+gsap.fromTo(projectsHTML.children, { y: '+=100', opacity: 0 }, { y: 0, opacity: 1, stagger: 0.3, duration: 1, scrollTrigger: { trigger: '.projects', start: "top 40%" } });
+gsap.fromTo(contactHTML.children, { y: '+=100', opacity: 0 }, { y: 0, opacity: 1, stagger: 0.3, duration: 1, scrollTrigger: { trigger: '.contact', start: "top -50%" } });
 
 
 const projectScheme = (name, description, githuburl, homepage) => {
@@ -41,11 +55,12 @@ fetch("https://api.github.com/users/misypniewski/repos")
   .then(resp => {
     resp.forEach(item => {
       if (item.description != null) {
-        projectsHTML.innerHTML += projectScheme(item.name, item.description, item.html_url, item.homepage);
+        projectsWrapperHTML.innerHTML += projectScheme(item.name, item.description, item.html_url, item.homepage);
       }
 
     });
   })
   .catch((error) => console.log(error));
+
 
 
