7c4aad0a891ed295c0d149f0b0dc0a89589b65fd
MiSypniewski <mi.sypniewski@gmail.com>
2020-08-11 22:03:47 +0200

    FEAT: save projets in local storage, disable animation on mobile

--- src/js/index.js
+++ src/js/index.js
@@ -1,40 +1,40 @@
 import "../scss/main.scss";
 import { gsap } from "gsap";
 import { ScrollTrigger } from "gsap/ScrollTrigger";
-
 gsap.registerPlugin(ScrollTrigger);
 
-console.log("HELLO 👋");
-//https://api.github.com/users/:username/repos
-
+const date = new Date().toISOString().slice(0, 10);
+const projects = [];
 const projectsWrapperHTML = document.querySelector(".projects__wrapper--js");
 
+const width = window.innerWidth;
 const animationSections = document.querySelectorAll(".animation--js");
-
 const heroHandHTML = document.querySelector(".hero__hand");
 const heroBubbleHTML = document.querySelector(".hero__bubble");
 
-gsap.fromTo(heroBubbleHTML, { scale: 0 }, { scale: 1, transformOrigin: "0% 100%", duration: 0.3, delay: 0.5 });
-gsap.fromTo(
-  heroHandHTML,
-  { rotate: -10 },
-  { rotate: 10, ease: "easeInOut", transformOrigin: "80% 80%", duration: 0.3, repeat: -1, yoyo: true }
-);
-
-animationSections.forEach((section) => {
+if (width >= 768) {
+  gsap.fromTo(heroBubbleHTML, { scale: 0 }, { scale: 1, transformOrigin: "0% 100%", duration: 0.3, delay: 0.5 });
   gsap.fromTo(
-    section.children,
-    { y: "+=100", opacity: 0 },
-    {
-      y: 0,
-      opacity: 1,
-      stagger: 0.3,
-      duration: 0.8,
-      ease: "easeInOut",
-      scrollTrigger: { trigger: section, start: "top 40%" },
-    }
+    heroHandHTML,
+    { rotate: -10 },
+    { rotate: 10, ease: "easeInOut", transformOrigin: "80% 80%", duration: 0.3, repeat: -1, yoyo: true }
   );
-});
+
+  animationSections.forEach((section) => {
+    gsap.fromTo(
+      section.children,
+      { y: "+=100", opacity: 0 },
+      {
+        y: 0,
+        opacity: 1,
+        stagger: 0.3,
+        duration: 0.8,
+        ease: "easeInOut",
+        scrollTrigger: { trigger: section, start: "top 50%" },
+      }
+    );
+  });
+}
 
 const projectScheme = (name, description, githuburl, homepage) => {
   return `<article class="project">
@@ -65,13 +65,25 @@ const projectScheme = (name, description, githuburl, homepage) => {
         </article>`;
 };
 
-fetch("https://api.github.com/users/misypniewski/repos")
-  .then((resp) => resp.json())
-  .then((resp) => {
-    resp.forEach((item) => {
-      if (item.description != null) {
-        projectsWrapperHTML.innerHTML += projectScheme(item.name, item.description, item.html_url, item.homepage);
-      }
-    });
-  })
-  .catch((error) => console.log(error));
+if (localStorage.getItem("date") !== date) {
+  fetch("https://api.github.com/users/misypniewski/repos")
+    .then((resp) => resp.json())
+    .then((resp) => {
+      resp.forEach((item) => {
+        if (item.description != null) {
+          projects.push(item);
+          projectsWrapperHTML.innerHTML += projectScheme(item.name, item.description, item.html_url, item.homepage);
+        }
+        localStorage.setItem("projects", JSON.stringify(projects));
+        localStorage.setItem("date", date);
+      });
+    })
+    .catch((error) => console.log(error));
+} else {
+  const projects = JSON.parse(localStorage.getItem("projects"));
+  projects.forEach((item) => {
+    projectsWrapperHTML.innerHTML += projectScheme(item.name, item.description, item.html_url, item.homepage);
+  });
+}
+
+console.log("HELLO 👋");
